4d7910238a5837769905f07e9aacf896f7065f8d
Egomessss <ed.gomes97@gmail.com>
2023-01-20 15:36:55 +0000

    finished with filtercarousel and data for cards

--- src/components/SwipeCarouselFilter.tsx
+++ src/components/SwipeCarouselFilter.tsx
@@ -18,20 +18,21 @@ function SwipeCarouselFilter() {
       spaceBetween={1}
       freeMode={true}
       modules={[FreeMode]}
-      className="mySwiper"
+      className="flex justify-between items-center border-b-2 drop-shadow-md
+      "
     >
       {data.imgs.map((img) => {
         return (
           <SwiperSlide
             key={img.id}
-            className="flex flex-col items-center h-[74px] justify-between"
+            className="flex flex-col items-center h-[74px] p-2 hover:underline active:underline focus:underine focus:underline"
           >
             <img
               className="h-[28px]"
               alt={img.title}
               src={img.link}
             />
-            <p className="text-xs mt-1">{img.title}</p>
+            <p className="text-xs mt-1">Contryside</p>
           </SwiperSlide>
         )
       })}
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import HomepageCard from "../components/HomepageCard"
 import Navbar from "../components/Navbar"
 import ScrollFilter from "../components/ScrollFilter"
 
@@ -7,6 +8,7 @@ function Homepage() {
     <div>
       <Navbar />
       <ScrollFilter />
+      <HomepageCard/>
     </div>
   )
 }
