ddfad47368a01bdb27a89b126212561a5cb414f0
Egomessss <ed.gomes97@gmail.com>
2023-01-24 18:06:01 +0000

    added fixed button component

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,17 +1,20 @@
+import { MapIcon } from "@heroicons/react/24/solid"
 import React from "react"
 import Footer from "../components/Footer"
 import HomepageCard from "../components/HomepageCard"
 import Navbar from "../components/Navbar"
+import StickyButton from "../components/StickyButton"
 import SwipeCarouselFilter from "../components/SwipeCarouselFilter"
 
 function Homepage() {
   return (
-    <div>
+    <div className="relative">
       <div className="sticky top-0 z-10 bg-white">
         <Navbar />
         <SwipeCarouselFilter />
       </div>
       <HomepageCard />
+      <StickyButton />
       <Footer />
     </div>
   )
