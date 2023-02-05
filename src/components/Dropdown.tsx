4ebb14f6a2f6eff60deddaa62797df2c7b4f4ca2
Egomessss <ed.gomes97@gmail.com>
2023-02-05 19:52:31 +0000

    dropdown menu icon changed

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -3,7 +3,7 @@ import React from "react"
 import { Fragment } from "react"
 import { Menu, Transition } from "@headlessui/react"
 
-import { Bars3Icon, UserCircleIcon } from "@heroicons/react/24/outline"
+import { Bars3Icon} from "@heroicons/react/24/outline"
 
 export default function Dropdown() {
   const aStyle =
@@ -16,7 +16,9 @@ export default function Dropdown() {
       <div>
         <Menu.Button className="flex items-center justify-between border-gray-200 border-2 p-2 rounded-3xl gap-2 h-[40px] w-[72px] cursor-pointer hover:shadow-lg">
           <Bars3Icon className="h-6" />
-          <UserCircleIcon className="h-7" />
+          <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation" focusable="false" 
+          className="block h-8 w-8 fill-[#717171]"><path d="m16 .7c-8.437 0-15.3 6.863-15.3 15.3s6.863 15.3 15.3 15.3 15.3-6.863 15.3-15.3-6.863-15.3-15.3-15.3zm0 28c-4.021 0-7.605-1.884-9.933-4.81a12.425 12.425 0 0 1 6.451-4.4 6.507 6.507 0 0 1 -3.018-5.49c0-3.584 2.916-6.5 6.5-6.5s6.5 2.916 6.5 6.5a6.513 6.513 0 0 1 -3.019 5.491 12.42 12.42 0 0 1 6.452 4.4c-2.328 2.925-5.912 4.809-9.933 4.809z"></path></svg>
+          
         </Menu.Button>
       </div>
 
