57846c77e54ce7580e97e387217352d5c7c898cd
Egomessss <ed.gomes97@gmail.com>
2023-02-07 23:20:29 +0000

    added react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,32 @@
-import Navbar from "./components/Navbar/Navbar/Navbar"
 import Homepage from "./pages/Homepage/index"
 import Listings from "./pages/ListingPage/index"
 import MapPage from "./pages/MapPage"
 import SearchPage from "./pages/SearchPage"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
 function App() {
   return (
     <div>
-      <Homepage />
-      {/* <Listings/> */}
-      {/* <SearchPage /> */}
-      {/* <MapPage/> */}
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={<Homepage />}
+          />
+          <Route
+            path="/Listings"
+            element={<Listings />}
+          />{" "}
+          <Route
+            path="/SearchPage"
+            element={<SearchPage />}
+          />{" "}
+          <Route
+            path="/MapPage"
+            element={<MapPage />}
+          />
+        </Routes>
+      </Router>
     </div>
   )
 }
