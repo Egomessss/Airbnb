55fbca53818a06d3a9f073733bef5bdbaaaaaf62
Egomessss <ed.gomes97@gmail.com>
2023-02-19 22:56:11 +0000

    listing grid responsiveness fixed

--- src/pages/ListingPage/ImageGrid.tsx
+++ src/pages/ListingPage/ImageGrid.tsx
@@ -1,4 +1,3 @@
-
 import React from "react"
 import ListingsData from "../../assets/ListingsData.json"
 
@@ -6,39 +5,43 @@ const data = ListingsData.beachfront
 
 function ImageGrid() {
   return (
-    <div className="h-[580px] w-full">
-      <ul className="grid grid-cols-4 grid-rows-2 gap-2">
-        <li className="row-span-2 col-span-2">
-          <img className="object-cover min-w-[560px] min-h-[560px]"
+    <div className="hidden  md:block">
+      <ul className=" grid min-h-[400px] grid-cols-4 grid-rows-2 gap-2">
+        <li className="col-span-2 row-span-2">
+          <img
+            className="min-h-[410px] object-cover"
             src={data.image1}
             alt=""
           />
         </li>
         <li className="col-span-1 row-span-1">
-          <img className="object-cover min-w-[272px] min-h-[280px]"
+          <img
+            className="min-h-[200px]  object-cover"
             src={data.image2}
             alt=""
           />
         </li>
         <li className="col-span-1 row-span-1">
-          <img className="object-cover min-w-[272px] min-h-[280px]"
+          <img
+            className="min-h-[200px]  object-cover"
             src={data.image3}
             alt=""
           />
         </li>
         <li className="col-span-1 row-span-1">
-          <img className="object-cover min-w-[272px] min-h-[280px]"
+          <img
+            className="min-h-[200px]  object-cover"
             src={data.image4}
             alt=""
           />
         </li>
         <li className="col-span-1 row-span-1">
-          <img className="object-cover min-w-[272px] min-h-[280px]"
+          <img
+            className="min-h-[200px]  object-cover"
             src={data.image5}
             alt=""
           />
         </li>
-        
       </ul>
     </div>
   )
