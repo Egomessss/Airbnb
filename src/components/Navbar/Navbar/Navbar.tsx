be14cdeb9438475c70d153966d352866795f5ca9
Egomessss <ed.gomes97@gmail.com>
2023-02-19 22:59:53 +0000

    fixed mobile navbar positioning

--- src/components/Navbar/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar/Navbar.tsx
@@ -7,7 +7,7 @@ import NavRightSide from "./NavRightSide"
 
 function Navbar() {
   return (
-    <nav className="flex items-center justify-between  border-b-[1.5px] px-4 py-2 md:pt-4 min-w-[400px]">
+    <nav className="flex items-center justify-between  border-b-[1.5px] px-4 py-2 md:pt-4  md:min-w-[400px]">
       {/* full scale */}
       <NavLeftSide />
       <NavMiddleFull /> 
