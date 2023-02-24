2274c3a86f859ecd7f7c6e709a6f02cc8827b15f
Egomessss <ed.gomes97@gmail.com>
2023-02-24 19:13:28 +0000

    got the search params to work

--- src/pages/SearchPage/index.tsx
+++ src/pages/SearchPage/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import { MapIcon } from "@heroicons/react/24/solid"
 
 import BottomNav from "../../components/BottomNav"
@@ -16,7 +16,7 @@ import SearchPagination from "./SearchPagination"
 
 import Listings from "../../components/Listings"
 import ListingData from "../../assets/ListingsData.json"
-import { useSearchParams } from "react-router-dom"
+import { useLocation, useSearchParams } from "react-router-dom"
 import { format } from "date-fns"
 
 function SearchPage() {
@@ -27,9 +27,87 @@ function SearchPage() {
     setOpenMap((prevMode) => !prevMode)
   }
 
-  // ! pagination
+ 
+
+  //! filter by location type settings
+  // const [searchParams, setSearchParams] = useSearchParams()
+
+  // const filter = searchParams.get("filter")
+
+  // function setFilter(filter) {
+  //   setSearchParams({ filter: filter })
+  // }
+
+  // const filteredData = currentPosts.filter((listing) => {
+  //   !filter || listing.type_of_location === filter
+  // })
+
+  //  search params
+
+  // const [searchParams, setSearchParams] = useSearchParams()
+  // const startDate = searchParams.get("startDate")
+  // const endDate = searchParams.get("endDate")
+
+  // const location = searchParams.get("location")
+  // const guests = searchParams.get("guests")
+  // const formattedStartDate = format(new Date(startDate), "dd MMMM yy")
+  // const formattedendDate = format(new Date(endDate), "dd MMMM yy")
+
+  // const range = `${formattedStartDate} - ${formattedendDate}`
+
+  //  now i need to match this search params to the listing data and filter it
+  // add the setFilter function the navbar
+
+  // add an object query?
+
+  // im flexible needs to return all
+  //date needs to return two matchable string range?
+  // guests needs to be
+
+  const location = useLocation()
+  const searchParams = new URLSearchParams(location.search)
+
+  const locationParam = searchParams.get("location")
+  console.log(locationParam)
+  const guestsParam = searchParams.get("guests")
+
+  const startDateParam = searchParams.get("startDate")
+  const endDateParam = searchParams.get("endDate")
+  const formattedStartDate = format(new Date(startDateParam), "dd MMMM yy")
+  const formattedEndDate = format(new Date(endDateParam), "dd MMMM yy")
+
+  const [filteredData, setFilteredData] = useState([])
+  console.log(filteredData)
+
+
+  useEffect(() => {
+    // Filter the data based on the query parameters
+    const filteredDataParams = ListingData.filter((item) => {
+      return (
+        item.location === locationParam 
+        // && 
+        // item.accommodates <= guestsParam
+        // item.startDate >= startDateParam &&
+        // item.endDate <= endDateParam
+      )
+    })
+    // Set the filtered data in the state
+    setFilteredData(filteredDataParams)
+  }, [locationParam, guestsParam, startDateParam, endDateParam])
+
+
+
+  const range = `${formattedStartDate} - ${formattedEndDate}`
+
+  // const filteredParamsData = currentPosts.filter((listing) => {
+  //   !filter || listing.type_of_location === filter ||
+  //     listing.location === location
+  // })
+
+
+   // ! pagination
   // fetches the data from the json file
-  const [posts] = useState(ListingData)
+  const [posts] = useState(filteredData)
 
   // sets the current page and the post per page in the pagination component
   const [currentPage, setCurrentPage] = useState(1)
@@ -62,50 +140,21 @@ function SearchPage() {
   for (let i = 1; i <= Math.ceil(posts.length / postsPerPage); i++) {
     pageNumbers.push(i)
   }
-
-  // filter settings
-  const [searchParams, setSearchParams] = useSearchParams()
-
-  const filter = searchParams.get("filter")
-
-  function setFilter(filter) {
-    setSearchParams({ filter: filter })
-  }
-
-  //  search params
-
-  const location = searchParams.get("location")
-  const startDate = searchParams.get("startDate")
-  const endDate = searchParams.get("endDate")
-  const guests = searchParams.get("guests")
-
-  const formattedStartDate = format(new Date(startDate), "dd MMMM yy")
-  const formattedendDate = format(new Date(endDate), "dd MMMM yy")
-
-  const range = `${formattedStartDate} - ${formattedendDate}`
-
-  //  now i need to match this search params to the listing data and filter it
-// add the setFilter function the navbar
-  const filteredData = currentPosts.filter(
-    (listing) =>
-      !filter || listing.type_of_location === filter || listing.city === location
-  )
-
   return (
     <nav className="md:px-10">
       {/* top section  Navigation and filter */}
       <Navbar />
-      <SwipeCarouselFilter
+      {/* <SwipeCarouselFilter
         ListingData={ListingData}
         setFilter={setFilter}
-      />
+      /> */}
 
       {/* left section in full and middle in mobile*/}
       <main className="flex md:relative ">
         {/* full screen listing data */}
         <div className="hidden px-4 md:inline-block lg:w-[60%]">
           <p className="pb-4 text-sm font-medium">
-            Over 8 homes in {location} - {range} - for {guests} guests
+            Over 8 homes in {locationParam} - {range} - for {guestsParam} guests
           </p>
           <Listings data={filteredData} />
           {/* <SearchCard currentPosts={currentPosts} /> */}
