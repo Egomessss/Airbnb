fd9e63f3255cf738d067697e99b726d00547747e
Egomessss <ed.gomes97@gmail.com>
2023-02-28 22:04:22 +0000

    added home link to bottom nav

--- src/components/BottomNav.tsx
+++ src/components/BottomNav.tsx
@@ -3,6 +3,7 @@ import {
   MagnifyingGlassIcon,
   UserCircleIcon,
 } from "@heroicons/react/24/outline"
+import { Link } from "react-router-dom"
 
 export default function BottomNav() {
   const aStyle =
@@ -10,34 +11,23 @@ export default function BottomNav() {
   const iconStyle = "h-6 w-6 mx-2 inline-block mb-1"
   const spanStyle = "tab tab-explore block text-xs focus:text-black"
 
-
-
-
-  
-
   return (
     <div className="fixed inset-x-0 bottom-0 flex w-full items-center justify-center bg-white py-2 shadow md:hidden">
-      <a
-        href="#"
-        className={aStyle}
-      >
-        <MagnifyingGlassIcon className={iconStyle} />
-        <span className={spanStyle}>Explore</span>
-      </a>
-      <a
-        href="#"
-        className={aStyle}
-      >
+      <Link to="/">
+        <button>
+          {" "}
+          <MagnifyingGlassIcon className={iconStyle} />
+          <span className={spanStyle}>Explore</span>
+        </button>
+      </Link>
+      <button className={aStyle}>
         <HeartIcon className={iconStyle} />
         <span className={spanStyle}>Wishlist</span>
-      </a>
-      <a
-        href="#"
-        className={aStyle}
-      >
+      </button>
+      <button className={aStyle}>
         <UserCircleIcon className={iconStyle} />
         <span className={spanStyle}>Log in</span>
-      </a>
+      </button>
     </div>
   )
 }
