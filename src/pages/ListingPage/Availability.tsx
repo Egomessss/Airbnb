e1a1a08eebc19a0851e2d4ca5a5b705a3067c951
Egomessss <ed.gomes97@gmail.com>
2023-02-28 00:44:39 +0000

    availability fixed, added the price, and rendering of values

--- src/pages/ListingPage/Availability.tsx
+++ src/pages/ListingPage/Availability.tsx
@@ -30,7 +30,8 @@ function Availability({ data }) {
     }
   }
 
-  const handleGuestPopOver = () => setGuestPopOver(prevGuestPopOver => !prevGuestPopOver)
+  const handleGuestPopOver = () =>
+    setGuestPopOver((prevGuestPopOver) => !prevGuestPopOver)
 
   // !checkin/out and calendar data
 
@@ -62,7 +63,7 @@ function Availability({ data }) {
 
   // formats the data so it can be shown in the navbar when a user select from the range picker
   const formattedStartDate = format(new Date(startDate), "P")
-  const formattedendDate = format(new Date(endDate), "P")
+  const formattedEndDate = format(new Date(endDate), "P")
 
   // calculate the number of days between the start and end date
   const daysInBetween = Math.round(
@@ -73,13 +74,19 @@ function Availability({ data }) {
 
   // console.log(daysInBetween)
 
-  // !Price
+  // !Price breakdown
 
-  const [price, setPrice] = useState(data.price_per_night)
+  const price = data.price_per_night
 
-  const priceTotal = () => {
-    price * daysInBetween
-  }
+  const pricePerGuest = data.price_per_guest * guests
+
+  const serviceFee = data.service_fee
+
+  const cleaningfee = data.cleaning_fee
+
+  const accomodationPrice = price * daysInBetween + pricePerGuest
+
+  const priceTotal = serviceFee + cleaningfee + accomodationPrice
 
   // ? add minimum date for the calendar
   // ? add a price total after we set the range
@@ -89,9 +96,9 @@ function Availability({ data }) {
 
   return (
     <div className="sticky top-2 hidden flex-col  bg-scroll md:flex ">
-      <div className=" mt-6 h-[302px] w-[372px] gap-5 rounded-xl border-[1px]  p-4 shadow-lg">
+      <div className=" mt-6 h-[452px] w-[372px] gap-5 rounded-xl border-[1px]  p-4 shadow-lg">
         <div className="flex h-16 items-center justify-between">
-          <h1 className="text-xl font-medium">{price}€ night</h1>
+          <h1 className="text-xl font-medium">{price}€ per night</h1>
           <div>
             <ul className="flex items-center gap-2 text-sm font-medium">
               <li>
@@ -114,8 +121,13 @@ function Availability({ data }) {
               <p className="font-medium uppercase">Checkout</p>
             </div>
             <div className="flex justify-around">
-              <p>add dates</p>
-              <p>add dates</p>
+              <p>
+                {/* if the accomodation price is higher than the price per night it means the user has selected the dates so display it otherside show the placeholder */}
+                {accomodationPrice > price ? formattedStartDate : "Add dates"}
+              </p>
+              <p>
+                {accomodationPrice > price ? formattedEndDate : "add dates"}
+              </p>
             </div>
           </button>
 
@@ -124,7 +136,9 @@ function Availability({ data }) {
             className="col-span-2  border-t-[1px] border-gray-500 px-14 text-left"
           >
             <p className="font-medium uppercase">Guests</p>
-            <p>1 guest</p>
+            <p>
+              {guests} {guests > 1 ? "guests " : "guest"}
+            </p>
           </button>
         </div>
         <div className="mt-4">
@@ -132,71 +146,91 @@ function Availability({ data }) {
             Reserve
           </button>
         </div>
+        <div>
+          <ul className="mt-4 flex flex-col gap-4">
+            <li className="flex justify-between">
+              <p className="underline">Accomodation</p>
+              <p>{accomodationPrice}€</p>
+            </li>
+            <li className="flex justify-between">
+              <p className="underline">Cleaning fee</p>
+              <p>{cleaningfee}€</p>
+            </li>
+            <li className="flex justify-between border-b-2 pb-5">
+              <p className="underline">Service fee</p>
+              <p>{serviceFee}€</p>
+            </li>
+            <li className="flex justify-between">
+              <p className="font-bold">Total</p>
+              <p>{priceTotal}€</p>
+            </li>
+          </ul>
+        </div>
         {datePopOver && (
-        <div className="absolute top-24 right-0 z-50 rounded-xl border-[1px] bg-white p-6 shadow-lg">
-          <div className="flex justify-between">
-            <div>
-              <p>{daysInBetween} nights</p>
-              <p>Minimum stay: </p>
+          <div className="absolute top-24 right-0 z-50 rounded-xl border-[1px] bg-white p-6 shadow-lg">
+            <div className="flex justify-between">
+              <div>
+                <p>{daysInBetween} nights</p>
+                <p>Minimum stay: </p>
+              </div>
+              <div className="flex w-[300px] gap-4 rounded-lg border-[1px] p-2">
+                <button className=" w-1/2 border-r-[1px] border-gray-500 px-2 text-left">
+                  <p className="text-[10px] font-medium  uppercase">Check-in</p>
+                  <p>{formattedStartDate}</p>
+                </button>
+                <button className=" w-1/2  border-gray-500 px-2 text-left">
+                  <p className="text-[10px] font-medium uppercase">Checkout</p>
+                  <p>{formattedEndDate}</p>
+                </button>
+              </div>
             </div>
-            <div className="flex w-[300px] gap-4 rounded-lg border-[1px] p-2">
-              <button className=" w-1/2 border-r-[1px] border-gray-500 px-2 text-left">
-                <p className="text-[10px] font-medium  uppercase">Check-in</p>
-                <p>{formattedStartDate}</p>
-              </button>
-              <button className=" w-1/2  border-gray-500 px-2 text-left">
-                <p className="text-[10px] font-medium uppercase">Checkout</p>
-                <p>{formattedendDate}</p>
-              </button>
-            </div>
-          </div>
 
-          <DateRangePicker
-            minDate={new Date()}
-            maxDate={maxDate}
-            staticRanges={[]}
-            inputRanges={[]}
-            showDateDisplay={false}
-            months={2}
-            direction="horizontal"
-            ranges={[selectionRange]}
-            onChange={handleSelection}
-          />
-          <div className=" flex justify-between">
-            <CiKeyboard className="text-3xl" />
-            <div className="flex gap-3">
-              <button
-                onClick={clearSelection}
-                className="font-medium underline"
-              >
-                Clear dates
-              </button>
-              <button
-                onClick={closeBtn}
-                className="rounded-lg bg-black py-1 px-3 font-semibold text-white"
-              >
-                Close
-              </button>
+            <DateRangePicker
+              minDate={new Date()}
+              maxDate={maxDate}
+              staticRanges={[]}
+              inputRanges={[]}
+              showDateDisplay={false}
+              months={2}
+              direction="horizontal"
+              ranges={[selectionRange]}
+              onChange={handleSelection}
+            />
+            <div className=" flex justify-between">
+              <CiKeyboard className="text-3xl" />
+              <div className="flex gap-3">
+                <button
+                  onClick={clearSelection}
+                  className="font-medium underline"
+                >
+                  Clear dates
+                </button>
+                <button
+                  onClick={closeBtn}
+                  className="rounded-lg bg-black py-1 px-3 font-semibold text-white"
+                >
+                  Close
+                </button>
+              </div>
             </div>
           </div>
-        </div>
-      )}
+        )}
         {guestPopOver && (
-        <div className="absolute top-[201px] border-[1px] px-4 drop-shadow-lg bg-white z-50 flex w-[340px] rounded-xl items-center justify-between py-6">
-          <h2 className="font-semibold">Guests</h2>
-          <div className="flex items-center gap-4">
-            <MinusIcon
-              onClick={handleDecrementClickGuests}
-              className="h-10 rounded-full border-2 p-2"
-            />
-            <p>{guests}</p>
-            <PlusIcon
-              onClick={handleIncrementClickGuests}
-              className="h-10 rounded-full border-2 p-2"
-            />
+          <div className="absolute top-[201px] z-50 flex w-[340px] items-center justify-between rounded-xl border-[1px] bg-white px-4 py-6 drop-shadow-lg">
+            <h2 className="font-semibold">Guests</h2>
+            <div className="flex items-center gap-4">
+              <MinusIcon
+                onClick={handleDecrementClickGuests}
+                className="h-10 rounded-full border-2 p-2"
+              />
+              <p>{guests}</p>
+              <PlusIcon
+                onClick={handleIncrementClickGuests}
+                className="h-10 rounded-full border-2 p-2"
+              />
+            </div>
           </div>
-        </div>
-      )}
+        )}
       </div>
       {data.isRareFind ? (
         <div className=" mt-6 flex h-[92px] w-[372px] items-center justify-center  gap-2 rounded-xl border-[1px]  bg-scroll p-2 shadow-lg ">
@@ -209,9 +243,6 @@ function Availability({ data }) {
           <SlDiamond className="block h-[32px] w-[32px] fill-[#E31C5F]" />
         </div>
       ) : null}
-
-      
-      
     </div>
   )
 }
