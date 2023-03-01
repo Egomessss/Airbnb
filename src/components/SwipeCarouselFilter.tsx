fabb5db7fa916a757ce7e319146438e433ff2fef
Egomessss <ed.gomes97@gmail.com>
2023-03-01 20:38:29 +0000

    added filters json for the swipecarousel filter and removed search card component

--- src/components/SwipeCarouselFilter.tsx
+++ src/components/SwipeCarouselFilter.tsx
@@ -17,54 +17,12 @@ import {
 } from "@heroicons/react/24/outline"
 
 import ListingData from "../assets/ListingsData.json"
+import filters from "../assets/filters.json"
 import { Link, useSearchParams } from "react-router-dom"
 // each search parameter needs to match their title to the search query
 
 function SwipeCarouselFilter(props) {
   const [openFilter, setOpenFilter] = useState(false)
-
-  const filters = [
-    {
-      id: 2,
-      title: "Beachfront",
-      link: "https://a0.muscache.com/pictures/bcd1adc0-5cee-4d7a-85ec-f6730b0f8d0c.jpg",
-    },
-    {
-      id: 3,
-      title: "Cabin",
-      link: "https://a0.muscache.com/pictures/732edad8-3ae0-49a8-a451-29a8010dcc0c.jpg",
-    },
-    {
-      id: 4,
-      title: "LakeSide",
-      link: "https://a0.muscache.com/pictures/a4634ca6-1407-4864-ab97-6e141967d782.jpg",
-    },
-    {
-      id: 5,
-      title: "Tropical",
-      link: "https://a0.muscache.com/pictures/ee9e2a40-ffac-4db9-9080-b351efc3cfc4.jpg",
-    },
-    {
-      id: 6,
-      title: "Countryside",
-      link: "https://a0.muscache.com/pictures/6ad4bd95-f086-437d-97e3-14d12155ddfe.jpg",
-    },
-    {
-      id: 7,
-      title: "Mansions",
-      link: "https://a0.muscache.com/pictures/78ba8486-6ba6-4a43-a56d-f556189193da.jpg",
-    },
-    {
-      id: 8,
-      title: "Skiing",
-      link: "https://a0.muscache.com/pictures/c8bba3ed-34c0-464a-8e6e-27574d20e4d2.jpg",
-    },
-    {
-      id: 9,
-      title: "Iconic cities",
-      link: "https://a0.muscache.com/pictures/ed8b9e47-609b-44c2-9768-33e6a22eccb2.jpg",
-    },
-  ]
 
   return (
     <div className="mx-auto hidden w-[600px] items-center justify-center gap-2 py-6 md:flex">
@@ -74,7 +32,6 @@ function SwipeCarouselFilter(props) {
         navigation={true}
         modules={[Navigation]}
       >
-       
         <SwiperSlide className="my-2 flex h-[51px] w-[77px]  flex-col flex-wrap items-center justify-center decoration-2 underline-offset-8 hover:underline hover:decoration-gray-300  focus:underline focus:decoration-black">
           <Link
             to={`/ListingPage/${
@@ -133,7 +90,7 @@ function SwipeCarouselFilter(props) {
               <div>
                 <input type="range" />
               </div>
-              <div className="flex gap-2 items-center">
+              <div className="flex items-center gap-2">
                 <div className="relative">
                   <p className="absolute p-2 text-xs text-gray-400">
                     min price
@@ -160,7 +117,12 @@ function SwipeCarouselFilter(props) {
           </div>
           <div>
             <h3>Rooms and beds</h3>
-            
+          </div>
+          <div>
+            <h3>Type of place</h3>
+          </div>
+          <div>
+            <h3>property type</h3>
           </div>
           <div>
             <h3>Amenities</h3>
