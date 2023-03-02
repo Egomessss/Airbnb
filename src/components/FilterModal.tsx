6d7e6aeea49cc20d9b6a1176412939006fbeebcf
Egomessss <ed.gomes97@gmail.com>
2023-03-02 21:54:58 +0000

    filter modal fully responsive and working as a separate component

--- src/components/FilterModal.tsx
+++ src/components/FilterModal.tsx
@@ -1,11 +1,11 @@
-import { XMarkIcon } from '@heroicons/react/24/outline'
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { XMarkIcon } from "@heroicons/react/24/outline"
+import React, { useState } from "react"
+import { Link } from "react-router-dom"
 import amenities from "../assets/amenities.json"
 import ListingData from "../assets/ListingsData.json"
 
-function FilterModal({closeFilterDropdown}) {
-// price breakdown
+function FilterModal({ closeFilterDropdown }) {
+  // price breakdown
 
   // extract the prices from the data
   const prices = ListingData.map((item) => item.price_per_night)
@@ -84,124 +84,117 @@ function FilterModal({closeFilterDropdown}) {
   }
 
   return (
-    <div className="absolute h-screen w-full inset-0 md:top-44 md:h-[600px] md:w-[600px] overflow-y-scroll rounded-lg border-[1px] bg-white px-4 ">
-          <div className="flex h-14 items-center justify-between border-b-2 ">
-            <button onClick={closeFilterDropdown}>
-              <XMarkIcon className="h-5" />
-            </button>
-            <p>Filters</p>
-            {/* empty html tag so we can justify between technique utilized by the airbnb folks */}
-            <p></p>
-          </div>
-          {/* price filter */}
-          <div className="h-[170px] border-b-2">
-            <h2 className=" pt-4 font-semibold">Price range</h2>
-            <p>The average nightly price is €{medianPrice}</p>
-            <div className="flex flex-col items-center">
-              <div className="flex items-center gap-2">
-                <div className="relative py-4">
-                  <p className="absolute p-2 text-xs text-gray-400">
-                    min price €
-                  </p>
-                  <input
-                    className="h-14  w-[145px] md:w-[270px] rounded-lg border-[1px] pt-2 pl-2"
-                    type="number"
-                    name="minPrice"
-                    min={lowestPrice}
-                    value={priceFilter.minPrice}
-                    onChange={handlePriceFilterChange}
-                  />
-                </div>
-                <p>-</p>
-                <div className="relative">
-                  <p className="absolute p-2 text-xs text-gray-400">
-                    max price €
-                  </p>
-                  <input
-                    className="h-14 w-[145px] md:w-[270px] rounded-lg border-[1px] pt-2 pl-2"
-                    type="number"
-                    name="maxPrice"
-                    max={highestPrice}
-                    value={priceFilter.maxPrice}
-                    onChange={handlePriceFilterChange}
-                  />
-                </div>
-              </div>
+    <div className="absolute top-0  h-screen w-full  overflow-y-scroll rounded-lg border-[1px] bg-white px-4 md:top-44 md:h-[600px] md:w-[600px] ">
+      <div className="flex h-14 items-center justify-between border-b-2 ">
+        <button onClick={closeFilterDropdown}>
+          <XMarkIcon className="h-5" />
+        </button>
+        <p>Filters</p>
+        {/* empty html tag so we can justify between technique utilized by the airbnb folks */}
+        <p></p>
+      </div>
+      {/* price filter */}
+      <div className="h-[170px] border-b-2">
+        <h2 className=" pt-4 font-semibold">Price range</h2>
+        <p>The average nightly price is €{medianPrice}</p>
+        <div className="flex flex-col items-center">
+          <div className="flex items-center gap-2">
+            <div className="relative py-4">
+              <p className="absolute p-2 text-xs text-gray-400">min price €</p>
+              <input
+                className="h-14  w-full min-w-[125px] rounded-lg border-[1px] pt-2 pl-2"
+                type="number"
+                name="minPrice"
+                min={lowestPrice}
+                value={priceFilter.minPrice}
+                onChange={handlePriceFilterChange}
+              />
+            </div>
+            <p>-</p>
+            <div className="relative">
+              <p className="absolute p-2 text-xs text-gray-400">max price €</p>
+              <input
+                className="h-14 w-full min-w-[125px] rounded-lg border-[1px] pt-2 pl-2"
+                type="number"
+                name="maxPrice"
+                max={highestPrice}
+                value={priceFilter.maxPrice}
+                onChange={handlePriceFilterChange}
+              />
             </div>
           </div>
-          {/* amenities section */}
-          <div className="py-4">
-            <h2 className=" pb-4 font-semibold">Amenities</h2>
-            <ul className="flex w-full flex-wrap gap-4">
-              {amenities.map((amenity, index) => {
-                return (
-                  <li
-                    key={index}
-                    className="w-[250px] "
-                  >
-                    <label
-                      key={index}
-                      className="flex items-center gap-3"
-                    >
-                      {/* creates a list of checkboxes for the amneties */}
-                      <input
-                        className="h-5 w-5"
-                        type="checkbox"
-                        value={amenity}
-                        checked={selectedAmenities.includes(amenity)}
-                        onChange={(e) =>
-                          e.target.checked
-                            ? // If the checkbox is checked , the setSelectedAmenities function is called with a new array that includes the current amenity in addition to the existing selected amenities.
-                              setSelectedAmenities([
-                                ...selectedAmenities,
-                                amenity,
-                              ])
-                            : // If the checkbox is unchecked , the setSelectedAmenities function is called with a new array that excludes the current amenity.
-                              setSelectedAmenities(
-                                // If a is not equal to amenity, the element is included in the new array. If a is equal to amenity, the element is excluded from the new array.
-                                selectedAmenities.filter((a) => a !== amenity)
-                              )
-                        }
-                      />
-                      {amenity}
-                    </label>
-                  </li>
-                )
-              })}
-            </ul>
-          </div>
-          {/* top tier section */}
-          <div className="py-4">
-            <h2 className="font-semibold">Top-tier stays</h2>
-            <label className="flex items-center gap-3 pt-4">
-              <input
-                className="h-5 w-5"
-                type="checkbox"
-                value="Superhost"
-                checked={superhost}
-                onChange={(e) => setSuperhost(e.target.checked)}
-              />
-              Superhost
-            </label>
-          </div>
-          <div className="inset-x-0 bottom-0 z-50 -mx-4 flex items-center justify-between rounded-lg border-t-[1px] bg-white py-4  px-4 shadow-xl">
-            <button
-              onClick={clearFilters}
-              className="font-semibold underline"
-            >
-              Clear all
-            </button>
-            <Link
-              to="/"
-              onClick={handleSubmit}
-            >
-              {" "}
-              <button className=" flex w-[100px] rounded-md bg-[#E21C61] p-2 font-semibold text-white">
-                show{}
-              </button>
-            </Link>
-          </div>
         </div>
+      </div>
+      {/* amenities section */}
+      <div className="py-4">
+        <h2 className=" pb-4 font-semibold">Amenities</h2>
+        <ul className="flex w-full flex-wrap gap-4">
+          {amenities.map((amenity, index) => {
+            return (
+              <li
+                key={index}
+                className="w-[250px] "
+              >
+                <label
+                  key={index}
+                  className="flex items-center gap-3"
+                >
+                  {/* creates a list of checkboxes for the amneties */}
+                  <input
+                    className="h-5 w-5"
+                    type="checkbox"
+                    value={amenity}
+                    checked={selectedAmenities.includes(amenity)}
+                    onChange={(e) =>
+                      e.target.checked
+                        ? // If the checkbox is checked , the setSelectedAmenities function is called with a new array that includes the current amenity in addition to the existing selected amenities.
+                          setSelectedAmenities([...selectedAmenities, amenity])
+                        : // If the checkbox is unchecked , the setSelectedAmenities function is called with a new array that excludes the current amenity.
+                          setSelectedAmenities(
+                            // If a is not equal to amenity, the element is included in the new array. If a is equal to amenity, the element is excluded from the new array.
+                            selectedAmenities.filter((a) => a !== amenity)
+                          )
+                    }
+                  />
+                  {amenity}
+                </label>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+      {/* top tier section */}
+      <div className="py-4">
+        <h2 className="font-semibold">Top-tier stays</h2>
+        <label className="flex items-center gap-3 pt-4">
+          <input
+            className="h-5 w-5"
+            type="checkbox"
+            value="Superhost"
+            checked={superhost}
+            onChange={(e) => setSuperhost(e.target.checked)}
+          />
+          Superhost
+        </label>
+      </div>
+      <div className="inset-x-0 bottom-0 z-50 -mx-4 flex items-center justify-between rounded-lg border-t-[1px] bg-white py-4  px-4 shadow-xl">
+        <button
+          onClick={clearFilters}
+          className="font-semibold underline"
+        >
+          Clear all
+        </button>
+        <Link
+          to="/"
+          onClick={handleSubmit}
+        >
+          {" "}
+          <button className=" flex w-[100px] rounded-md bg-[#E21C61] p-2 font-semibold text-white">
+            show{}
+          </button>
+        </Link>
+      </div>
+    </div>
   )
 }
 
