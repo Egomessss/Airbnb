330558d472c8eb4dddd3402a177699efc408d414
Egomessss <ed.gomes97@gmail.com>
2023-03-04 00:33:34 +0000

    added backbutton to listingNav

--- src/pages/ListingPage/ListingNav.tsx
+++ src/pages/ListingPage/ListingNav.tsx
@@ -3,19 +3,22 @@ import {
   ChevronLeftIcon,
   HeartIcon,
 } from "@heroicons/react/24/outline"
-
+import { useNavigate } from "react-router-dom"
 
 function ListingNav() {
+  const navigate = useNavigate()
+  const goBack = () => navigate(-1)
+
   return (
-    <div className="md:hidden flex h-[64px] items-center justify-between">
-      <a
-        href=""
+    <div className="flex h-[64px] items-center justify-between md:hidden">
+      <button
+        onClick={goBack}
         className="flex items-center"
       >
         <ChevronLeftIcon className="h-4" />
         <p className="text-sm font-semibold">Homes</p>
-      </a>
-      <div className="flex justify-around w-14">
+      </button>
+      <div className="flex w-14 justify-around">
         <ArrowUpTrayIcon className="h-4" />
         <HeartIcon className="h-4" />
       </div>
