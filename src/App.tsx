ff827bf47e33e984e698f2572c2ecd0a070a3e1b
Egomessss <ed.gomes97@gmail.com>
2023-03-05 18:52:36 +0000

    removed lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,26 @@
-import { lazy, Suspense } from "react"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
-const HomePage = lazy(() => import("./pages/HomePage/index"))
-const SearchPage = lazy(() => import("./pages/SearchPage/index"))
-const ListingPage = lazy(() => import("./pages/ListingPage/index"))
-
-
+import HomePage from "./pages/HomePage/index"
+import SearchPage from "./pages/SearchPage"
+import ListingPage from "./pages/ListingPage/index"
 
 function App() {
   return (
     <Router>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route
-            path="/"
-            element={<HomePage />}
-          />
-          <Route
-            path="/searchPage"
-            element={<SearchPage />}
-          />
-          <Route
-            path="/listingpage/:listingId"
-            element={<ListingPage />}
-          />
-        </Routes>
-      </Suspense>
+      <Routes>
+        <Route
+          path="/"
+          element={<HomePage />}
+        />
+        <Route
+          path="/searchPage"
+          element={<SearchPage />}
+        />
+        <Route
+          path="/listingpage/:listingId"
+          element={<ListingPage />}
+        />
+      </Routes>
     </Router>
   )
 }
